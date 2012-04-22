02502fa0db64e7e12749fbc463069b600e913121
Addam Hardy <addam.hardy@gmail.com>
2012-04-22 12:42:57 -0500

    Initial Commit

---
+++ BullsEye/BullsEyeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BullsEyeAppDelegate.h
+//  BullsEye
+//
+//  Created by Addam on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BullsEyeViewController;
+
+@interface BullsEyeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BullsEyeViewController *viewController;
+
+@end
---
+++ BullsEye/BullsEyeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BullsEyeViewController.h
+//  BullsEye
+//
+//  Created by Addam on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BullsEyeViewController : UIViewController
+
+@end
